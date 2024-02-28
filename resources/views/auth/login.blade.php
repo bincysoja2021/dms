8e04c41f7d225ba27e611de95fc9f39eaf05318e
bincysoja2021 <bincysoja2017@gmail.com>
2024-02-28 18:02:59 +0530

    dashboard style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<title>DMS :: GTN Textiles</title>
+<link rel="icon" href="favicon.ico">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<!-- Bootstrap core CSS -->
+<link href="{{ asset ('css/bootstrap.min.css') }}" rel="stylesheet">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+<link rel="stylesheet" href="{{ asset ('font-awesome/css/font-awesome.min.css') }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<!-- Custom styles for this template -->
+<link href="{{ asset ('css/style.css') }}" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+</head>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+<body>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="login-cover">
+  <img src="{{ asset ('images/logo.svg') }}" class="login-logo">
+  <div class="login-box">
+    <h3>DMS Login</h3>
+    <div class="login-in">
+        <form method="POST" action="{{ route('login') }}">
+          @csrf
+          <label>Username</label>
+          <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Enter your username" name="email" required autocomplete="off">
+          @error('email')
+            <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+          <label>Password</label>
+          <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter your password" name="password" required autocomplete="off"> @error('password')
+            <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+            </span>
+           @enderror
+          <button type="submit" class="btn btn-primary btn-login">Login</button>
+        </form>
+          <a href="forgot-password.php">Forgot password?</a>
     </div>
+  </div>
+  <h6>
+    <i class="fa fa-copyright" aria-hidden="true"></i> 2024-25 GTN Enterprises.
+  </h6>
+  <h6>
+    GTN-DMS2024.V.0.1 by Exacore
+  </h6>
+  <h6>
+    DMS2024 App Info <i class="fa fa-info-circle" aria-hidden="true"></i>
+  </h6>  
 </div>
-@endsection
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-slim.min.js"><\/script>')</script>
+<!-- <script src="js/vendor/popper.min.js"></script>
+ -->
+<script src="{{ asset ('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
