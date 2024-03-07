a4e50eafe04168959a842ea061e03f904bc7f480
bincybabu <bincysoja2017@gmail.com>
2024-03-07 22:41:08 +0530

    msg validation

--- resources/views/admin/user/edit_user.blade.php
+++ resources/views/admin/user/edit_user.blade.php
@@ -128,12 +128,11 @@
         </table>
         <div class="btn-groups">
           <button type="submit"  class="btn btn-primary">Update</button>
+          @if(Auth::user()->user_type=="Super admin")
+          <button value="{{$data->id}}" class="btn btn-warning user_deactivate" id="deactivate_user">De-activate User</button>
+          @endif
         </div>
     </form>
-    <br>
-    @if(Auth::user()->user_type=="Super admin")
-    <button value="{{$data->id}}" class="btn btn-warning user_deactivate" id="deactivate_user">De-activate User</button>
-    @endif
     </div>
   </div>
 </div>
